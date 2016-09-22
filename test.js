8a143971e38c8d456be104df88160f1aa4f91783
Sindre Sorhus <sindresorhus@gmail.com>
2016-09-22 13:42:16 +0700

    ES2015ify and require Node.js 4

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 import test from 'ava';
-import codePointAt from 'code-point-at';
 import m from './';
 
 test(t => {
-	t.true(m(codePointAt('あ')));
-	t.true(m(codePointAt('谢')));
-	t.true(m(codePointAt('고')));
+	t.true(m('あ'.codePointAt(0)));
+	t.true(m('谢'.codePointAt(0)));
+	t.true(m('고'.codePointAt(0)));
+	t.false(m('a'.codePointAt(0)));
 	t.true(m(0x1f251));
-	t.false(m(codePointAt('a')));
 });
