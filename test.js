a6dcff80ac52871f0ce4a90401b5ec2f2ebf7631
xiaoyuhen <xiaoyuhen1994@gmail.com>
2018-04-25 11:43:57 +0800

    Improve test coverage

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ test(t => {
 	t.true(m('あ'.codePointAt(0)));
 	t.true(m('谢'.codePointAt(0)));
 	t.true(m('고'.codePointAt(0)));
+	t.false(m(NaN));
 	t.false(m('a'.codePointAt(0)));
 	t.true(m(0x1F251));
 });
